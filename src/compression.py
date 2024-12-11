8a0ddb59b9f168227522603a5dae15b0e2115d9f
vyeso <vitoryeso@gmail.com>
2024-12-11 12:33:46 -0300

    some coments to the compression functions

--- src/compression.py
+++ src/compression.py
@@ -3,7 +3,9 @@ import torch
 def pruning(model, gamma):
     if gamma > 0.0:
         with torch.no_grad():
-            betas = []
+            pruning_thresholds = []
+            # Aqui a gente pode calcular o desvio padrao da camada e também o desvio padrão da rede toda, ou por camadas mais estrategicamente escolhidas baseadas na arquitetura.
+            # so fazer por camada e global, ja seria muito interessante.
             for p in model.parameters():
                 # transformando o layer em um vetor unidimensional 
                 flatted_weights = torch.flatten(p)
@@ -12,23 +14,27 @@ def pruning(model, gamma):
                 std = torch.std(flatted_weights, -1)
 
                 # calculando o beta para a camada
-                beta = gamma * std                        
-                betas.append(beta)
-                
+                pruning_threshold = gamma * std                        
+                pruning_thresholds.append(pruning_threshold)
+
                 # mascara booleana, que indica qual peso devera ser mantido
-                mask = torch.gt(p.abs(), torch.ones_like(p) * beta)
+                mask = torch.gt(p.abs(), torch.ones_like(p) * pruning_threshold)
                 
                 # multiplicando os pesos da camada pelo tensor booleano
                 p.multiply_(mask)
                 #print(p)
                 #p[torch.logical_not(boolean)] = 0
 
-    return betas
+    return pruning_thresholds
 
+# quando varremos os parametros do modelo com model.parameters() temos que verificar o shape do tensor.
+# pq pode ser q o tensor seja so um unico valor como um bias por exemplo e nao vai fazer sentido o desvio padrao desse unico valor.
 def quantization(model, b, betas=[]):
     if b >= 1:
+        # aqui pode ser uma flag que diz se o pruning foi feito ou não
         if len(betas) > 0:
             with torch.no_grad():
+                
                 for p, beta in zip(model.parameters(), betas):
                     flatted_weights = torch.flatten(p)
                     qk_prime = (torch.max(torch.abs(flatted_weights)) - beta) / ( (2**(b - 1)) - 1 )
